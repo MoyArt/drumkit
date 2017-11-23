4b45b20627bc9a6ea12ec40cf40316f7be39b261
Moises <moises.pedraza.contractor@bbva.com>
2017-11-23 08:42:00 -0600

    JS Refactor on keydown event function

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,23 +2,20 @@ console.log('scripts loaded');
 /*
 We are going to create an event listener to listen when we press a key
 */
-window.addEventListener('keydown', function(e){
-/* Checking the event we can see that there´s one property that we can use, and that is 
-keyCode, e.keyCode returns the code of the key we are pressing so we can use it to create
-owr dom selector.
-We create a constant sound, and thats when we are going to create our slector.*/
+function playSound(e){
+    /* Checking the event we can see that theres one property that we can use, and that is keyCode, e.keyCode returns the code of the key we are pressing so we can use it to create
+    owr dom selector.
+    We create a constant sound, and thats when we are going to create our selector. */
     const audio = document.querySelector('audio[data-key=' + '"' + e.keyCode + '"'),
-          key = document.querySelector('.key[data-key=' + '"' + e.keyCode + '"');
+    key = document.querySelector('.key[data-key=' + '"' + e.keyCode + '"');
     if(!audio){
-        return // If there`s no sound attached to that key we are leaving the event.
-    }
-    /* we can play the audio using the play() method */
+        return // If theres no sound attached to that key we are leaving the event.
+    };
     audio.currentTime = 0; // We set the currentTime to 0 so everytime we press a key it starts.
     audio.play(); // we call the audio tag play Method to play the sound
     key.classList.add('playing'); //we add playing class to the key we pressed
     console.log(key)
-} )
-
+}
 const keys = document.querySelectorAll('.key');
 keys.forEach(function(e){
     e.addEventListener('transitionend', function(e){
@@ -28,4 +25,6 @@ keys.forEach(function(e){
         this.classList.remove('playing');
     });
 })
+
+window.addEventListener('keydown', playSound);
 
